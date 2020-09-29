11f37fdb57e7ea55b68dfe45c665a692cb2fff51
SergBrGHub <mbox4mail@gmail.com>
2020-09-29 23:13:04 +0300

    Updates

---
+++ precache-manifest.921aee504f21736322a83dfe9b16e9e2.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74df697a4b36285233c76ef50a3dc3fc",
+    "url": "/air-quality/index.html"
+  },
+  {
+    "revision": "2fb7d34560ddbcf20628",
+    "url": "/air-quality/static/css/2.e0686c58.chunk.css"
+  },
+  {
+    "revision": "f233694a6d707c132f67",
+    "url": "/air-quality/static/css/main.0be1c8af.chunk.css"
+  },
+  {
+    "revision": "2fb7d34560ddbcf20628",
+    "url": "/air-quality/static/js/2.c844eb71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/air-quality/static/js/2.c844eb71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f233694a6d707c132f67",
+    "url": "/air-quality/static/js/main.2ff23971.chunk.js"
+  },
+  {
+    "revision": "be8b00b45510cbbb35d0",
+    "url": "/air-quality/static/js/runtime-main.2c0b8cd0.js"
+  },
+  {
+    "revision": "0662d9de8af86fb66060a046fbafe1b5",
+    "url": "/air-quality/static/media/gc_logo.0662d9de.svg"
+  },
+  {
+    "revision": "4783cc43b217788871cd7233135c7965",
+    "url": "/air-quality/static/media/gc_logo_mob.4783cc43.svg"
+  },
+  {
+    "revision": "b236af76d3e12b9f2dc0a2d673375d86",
+    "url": "/air-quality/static/media/hero_bg.b236af76.jpg"
+  },
+  {
+    "revision": "2f311a05a79fb86cee6148d658aa8336",
+    "url": "/air-quality/static/media/itn_mix.2f311a05.svg"
+  },
+  {
+    "revision": "780cc2803523d297836cce598574e464",
+    "url": "/air-quality/static/media/itn_mix_mob.780cc280.svg"
+  },
+  {
+    "revision": "3d66b58c57513e79022af1cbb2936e5f",
+    "url": "/air-quality/static/media/politics_1.3d66b58c.jpg"
+  },
+  {
+    "revision": "fb06d4dd85a3187e7d299e96bd5393b8",
+    "url": "/air-quality/static/media/politics_2.fb06d4dd.jpg"
+  },
+  {
+    "revision": "a3fe3aa17418ceb9f8e4c1961faf77e0",
+    "url": "/air-quality/static/media/politics_3.a3fe3aa1.jpg"
+  },
+  {
+    "revision": "17b056bde9ed2137fe8aec437de3fec4",
+    "url": "/air-quality/static/media/politics_4.17b056bd.jpg"
+  },
+  {
+    "revision": "1d9ae9e91593976f92150a639bbdf321",
+    "url": "/air-quality/static/media/politics_5.1d9ae9e9.jpg"
+  },
+  {
+    "revision": "19cb675508b5ffec4024e651a6e5ec0e",
+    "url": "/air-quality/static/media/politics_6.19cb6755.jpg"
+  },
+  {
+    "revision": "341a487c2bb1cbed9179faa73d89b343",
+    "url": "/air-quality/static/media/shid_emozhl_logo.341a487c.svg"
+  },
+  {
+    "revision": "63671d590d956e7daf3defd867c6fc91",
+    "url": "/air-quality/static/media/shid_emozhl_logo_mob.63671d59.svg"
+  },
+  {
+    "revision": "7e1db0da3d25263be6534e000b29d2a8",
+    "url": "/air-quality/static/media/slide-1.7e1db0da.jpg"
+  },
+  {
+    "revision": "12a25e248f105a327021b69336c5b71b",
+    "url": "/air-quality/static/media/slide-2.12a25e24.jpg"
+  },
+  {
+    "revision": "638ec2275a294137e028e6bce58ad768",
+    "url": "/air-quality/static/media/slide-3.638ec227.jpg"
+  },
+  {
+    "revision": "8a21e8b7fc0908ab482cc0bb0c98138a",
+    "url": "/air-quality/static/media/slide-4.8a21e8b7.jpg"
+  },
+  {
+    "revision": "bb9f49bba9c380e18e77de4c1ed82bf6",
+    "url": "/air-quality/static/media/slide-5.bb9f49bb.jpg"
+  }
+]);
